63f8a0436f38d45c2aff3a404b5973ce1ac8b9a5
Jang Jaemin <jjjjj121@naver.com>
2022-09-05 16:34:36 +0900

    DNS 주석 설명 추가

--- main.cpp
+++ main.cpp
@@ -15,19 +15,22 @@ int main()
 	WSAData wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
+	//찾으려는 도메인 주소
+	hostent* infos = gethostbyname("naver.com");
 
-	hostent* infos = gethostbyname("Umamusume.com");
-
+	//공유하고 있는 별명?이 있는가?
 	for (int i = 0; infos->h_aliases[i]; ++i)
 	{
 		printf("alias %s\n", infos->h_aliases[i]);
 	}
 
+	//해당 도메인과 연결된 IP주소
 	for (int i = 0; infos->h_addr_list[i]; ++i)
 	{
 		printf("ip %s\n", inet_ntoa(*(IN_ADDR*)infos->h_addr_list[i]));
 	}
 
+	//IP주소 타입이 무엇인가?
 	printf("address type %s", infos->h_addrtype == AF_INET ? "AF_INET" : "AF_INET6");
 
 
